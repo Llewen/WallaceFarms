be1013a182f3f5e05ec641e4c72c62e676cc35be
Llewen <dave.newell@geneva.edu>
2014-02-04 20:43:43 -0500

    Added Index.chstml to Views\wallaceFarms\ folder
    The controller was unable to find the view if it was not contained
    within the folder with the same name as the controller under the Views
    folder.  Also edited the App_Start\RouteConfig.cs file to use the
    wallaceFarmsController.cs

--- WallaceFarms/App_Start/RouteConfig.cs
+++ WallaceFarms/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WallaceFarms
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "wallaceFarms", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
