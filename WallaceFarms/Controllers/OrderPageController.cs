deae6621b8fd3583b2018b0f7ba742b2ba88c2f3
simohuns <simohuns@gmail.com>
2014-04-15 20:54:10 -0400

    It Works
    The database, the backend, and the order form are all linked together.

--- WallaceFarms/Controllers/OrderPageController.cs
+++ WallaceFarms/Controllers/OrderPageController.cs
@@ -3,15 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WallaceFarms;
+using WallaceFarms.Models;
 
 namespace WallaceFarms.Controllers
 {
-    public class OrderPageController : Controller
+    public class OrderPageController : OrderController
     {
 
+        [HttpGet]
         public ActionResult Index()
         {
-            return View("OrderPage");
+            int ID;
+            int BeefID;
+
+            try
+            {
+                ID = (from o in entities.Orders select o.OrderID).Max();
+            }
+            catch
+            {
+                ID = 0;
+            }
+
+            try
+            {
+                BeefID = (from o in entities.BeefOrders select o.BeefOrderID).Max();
+            }
+            catch
+            {
+                BeefID = 0;
+            }
+            OrderModel order = new OrderModel();
+            order.theOrder = new Order();
+            order.theBeefOrder = new BeefOrder();
+
+            order.theOrder.OrderID = ID + 1;
+            order.theBeefOrder.BeefOrderID = BeefID + 1;
+            order.theBeefOrder.OrderID = order.theOrder.OrderID;
+            return View("OrderPage", order);
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(OrderModel Order)
+        {
+            entities.Orders.Add(Order.theOrder);
+            entities.BeefOrders.Add(Order.theBeefOrder);
+            entities.SaveChanges();
+
+            return Redirect("/Index");
         }
 
     }
