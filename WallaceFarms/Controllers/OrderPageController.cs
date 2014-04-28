152139ba27c1fa0d5ac349eb3524725b6e31163d
simohuns <simohuns@gmail.com>
2014-04-28 13:59:30 -0400

    Try Catch
    Add a try catch around the primary saveChanges() expression on the
    customer order page.  Helps define specifically what error is
    encountered when failing to save the input to the database.

--- WallaceFarms/Controllers/OrderPageController.cs
+++ WallaceFarms/Controllers/OrderPageController.cs
@@ -53,7 +53,32 @@ namespace WallaceFarms.Controllers
         {
             entities.Orders.Add(Order.theOrder);
             entities.BeefOrders.Add(Order.theBeefOrder);
-            entities.SaveChanges();
+            try
+            {
+                entities.SaveChanges();
+            }
+            // Handy code by Troy Alford and Richard of Stackoverflow.com
+            // Detects the specific database error and provides a brief description
+            catch (DbEntityValidationException ex)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var failure in ex.EntityValidationErrors)
+                {
+                    sb.AppendFormat("{0} failed validation\n", failure.Entry.Entity.GetType());
+                    foreach (var error in failure.ValidationErrors)
+                    {
+                        sb.AppendFormat("- {0} : {1}", error.PropertyName, error.ErrorMessage);
+                        sb.AppendLine();
+                    }
+                }
+
+                throw new DbEntityValidationException(
+                    "Entity Validation Failed - errors follow:\n" +
+                    sb.ToString(), ex
+                ); // Add the original exception as the innerException
+            }
+
 
             MailServer mailserver = new MailServer();
             mailserver.SendOrderMail(Order);
@@ -104,7 +129,6 @@ namespace WallaceFarms.Controllers
         public bool SendOrderMail(OrderModel order)
         {
             string body = order.theOrder.Name + " just ordered " + order.theBeefOrder.NumQuarters + " quarters of beef!";
-
             return SendMail("New Beef Order!", body);
         }
     }
